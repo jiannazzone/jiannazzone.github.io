f34c900f9472b62d03e530ca07d5900b9f99da83
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2023-01-13 09:44:48 -0500

    Stuff

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+function isMobile() {
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
 const menuItems = Array.from(document.getElementsByClassName('menu-item'));
 menuItems.forEach(menuItem => {
-    menuItem.addEventListener('mouseover', () => menuHover(menuItem));
-    menuItem.addEventListener('mouseout', () => menuExit(menuItem));
+    if (isMobile) {
+        menuItem.addEventListener('touchend', () => mobileClick(menuItem));
+        menuItem.style.pointerEvents = 'none';
+    } else {
+        menuItem.addEventListener('mouseover', () => menuHover(menuItem));
+        menuItem.addEventListener('mouseout', () => menuExit(menuItem));
+    }
 });
 
 const detailItems = document.getElementsByClassName('menu-image');
+
+function mobileClick(menuItem) {
+    const menuName = menuItem.innerHTML.toLowerCase();
+
+}
 
 function menuHover(menuItem) {
     const menuName = menuItem.innerHTML.toLowerCase();
