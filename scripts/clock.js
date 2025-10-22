29f1b800d36c7005f436219dcac7adbbba1d47f3
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2025-10-22 19:01:49 -0400

    Hide seconds when accessibility checked

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,8 +1,15 @@
 window.onload = displayClock();
 
 function displayClock() {
-    var clockText = new Date().toLocaleTimeString();
     clockElem = document.getElementById('clock');
+    const visualsCheckboxElem = document.getElementById('reduce-visuals-checkbox');
+    let clockText;
+
+    if (visualsCheckboxElem.checked) {
+        clockText = new Date().toLocaleTimeString([], {timeStyle: 'short'});
+    } else {
+        clockText = new Date().toLocaleTimeString();
+    }
     clockElem.innerHTML = clockText
     setTimeout(displayClock, 1000);
 }
