6e20edba4614654bbdbdc0555d865c530975f790
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2025-10-23 11:30:24 -0400

    Move window max/min logic into CSS

--- scripts/app.js
+++ scripts/app.js
@@ -1,47 +1,64 @@
 // Get all windows and add event listeners to the X
-const closeButtonElems = document.getElementsByClassName('window-closer')
-const maxButtonElems = document.getElementsByClassName('window-max');
-const minButtonElems = document.getElementsByClassName('window-min');
-const allButtons = document.getElementsByClassName('window-button');
+const closeButtonElems = document.querySelectorAll('.window-closer');
+const maxButtonElems = document.querySelectorAll('.window-max');
+const minButtonElems = document.querySelectorAll('.window-min');
 
-for (let i = 0; i < closeButtonElems.length; i++) {
-    closeButtonElems[i].addEventListener('click', function () {
-        const windowElem = document.getElementById(this.dataset.parent);
+closeButtonElems.forEach((el) => {
+    el.addEventListener('click', function () {
+        const windowElem = document.getElementById(el.dataset.parent);
         closeWindow(windowElem);
     });
-}
+});
 
-for (let i = 0; i < maxButtonElems.length; i++) {
-    maxButtonElems[i].addEventListener('click', function () {
-        const windowElem = document.getElementById(this.dataset.parent);
-        maximizeWindow(windowElem);
+maxButtonElems.forEach((el) => {
+    el.addEventListener('click', function() {
+        maximizeWindow(el);
     });
+});
+
+// Maximize Windows
+function maximizeWindow(button) {
+
+    // Swap button visibility
+    minButtonElems.forEach((el) => {
+        if (el.dataset.parent == button.dataset.parent) {
+            el.className = 'window-min window-button';
+        }
+    });
+    button.className += ' window-button-hidden';
+
+    // Adjust styles
+    const windowElem = document.getElementById(button.dataset.parent);
+    windowElem.className = 'window glow window-max';
 }
 
+// Minimize Windows
 for (let i = 0; i < minButtonElems.length; i++) {
     minButtonElems[i].addEventListener('click', function () {
-        const windowElem = document.getElementById(this.dataset.parent);
-        minimizeWindow(windowElem);
+        minimizeWindow(minButtonElems[i]);
     });
+}
+
+function minimizeWindow(button) {
+
+    // Swap button visibility
+    maxButtonElems.forEach((el) => {
+        if (el.dataset.parent == button.dataset.parent) {
+            el.className = 'window-max window-button';
+        }
+    });
+    button.className += ' window-button-hidden';
+
+    // Adjust styles
+    const windowElem = document.getElementById(button.dataset.parent);
+    windowElem.className = 'window glow draggable';
 }
 
 function closeWindow(windowElem) {
     windowElem.style.scale = '0';
 }
 
-function maximizeWindow(windowElem) {
-    windowElem.style.position = 'static';
-    windowElem.className = 'window glow';
-    windowElem.style.width = "100%";
-    windowElem.style.height = "100%";
-}
 
-function minimizeWindow(windowElem) {
-    windowElem.style.position = 'absolute';
-    windowElem.className += 'draggable';
-    windowElem.style.width = "66%";
-    windowElem.style.height = "50%";
-}
 
 // Window Dragging
 document.querySelectorAll('.window-header').forEach(header => {
